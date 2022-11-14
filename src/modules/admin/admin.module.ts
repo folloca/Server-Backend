3fb0f024a75000dabb07b6d691bc695a7c37aff4
mingi <skyup9393@gmail.com>
2022-11-14 22:17:51 +0900

    [build] #7 - Add dependency injection of repositories

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { TypeormRepositoryModule } from '../../database/typeorm-repository.module';
+import {
+  AdminRepository,
+  BannerRepository,
+} from '../../database/repositories/admin.repository';
+import { UserRepository } from '../../database/repositories/user.repository';
+import { HashTagRepository } from '../../database/repositories/hash-tag.repository';
 
 @Module({
+  imports: [
+    TypeormRepositoryModule.forTypeormRepository([
+      AdminRepository,
+      BannerRepository,
+      UserRepository,
+      HashTagRepository,
+    ]),
+  ],
   controllers: [AdminController],
   providers: [AdminService],
 })
--- src/modules/searches/searches.module.ts
+++ src/modules/searches/searches.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { SearchesController } from './searches.controller';
 import { SearchesService } from './searches.service';
+import { TypeormRepositoryModule } from '../../database/typeorm-repository.module';
+import { HashTagRepository } from '../../database/repositories/hash-tag.repository';
+import { EstateRepository } from '../../database/repositories/estate.repository';
+import { ProposalRepository } from '../../database/repositories/proposal.repository';
+import { LinkingRepository } from '../../database/repositories/linking.repository';
 
 @Module({
+  imports: [
+    TypeormRepositoryModule.forTypeormRepository([
+      HashTagRepository,
+      EstateRepository,
+      ProposalRepository,
+      LinkingRepository,
+    ]),
+  ],
   controllers: [SearchesController],
   providers: [SearchesService],
 })
