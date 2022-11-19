46eb245fb852d4b8c8bb909bf924a86cec4c240c
mingi <skyup9393@gmail.com>
2022-11-19 15:33:27 +0900

    [feat] #9 - Add email check API

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import { SignupDto } from './dto/signup.dto';
 
+@ApiTags('auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Get('/email-check/:email')
+  @ApiOperation({
+    summary: '이메일 중복 확인',
+    description: '이메일 중복 확인',
+  })
+  @ApiParam({
+    name: 'email',
+    type: String,
+    required: true,
+    description: '중복 검사하고자 하는 이메일 주소',
+  })
+  async emailCheck(@Param('email') email: string) {
+    return this.authService.emailCheck(email);
+  }
+
+  @Post()
+  @ApiOperation({
+    summary: '회원가입',
+    description: '이메일 회원가입',
+  })
+  @ApiBody({
+    schema: {
+      properties: {
+        email: { type: 'string' },
+        password: { type: 'string' },
+        marketingReception: { type: 'boolean' },
+      },
+    },
+  })
+  async signup(@Body() signupDto: SignupDto) {
+    const { email, password, marketingReception } = signupDto;
+    return this.authService.signup(email, password, marketingReception);
+  }
+}
