e5c716104f079eb8a5eb8ca0be9f8bf842641f31
mingi <skyup9393@gmail.com>
2022-11-21 09:38:47 +0900

    [feat] #9 - Add find account by email method

--- src/database/repositories/user.repository.ts
+++ src/database/repositories/user.repository.ts
@@ -3,4 +3,16 @@ import { Repository } from 'typeorm';
 import { UserEntity } from '../entities';
 
 @TypeormRepository(UserEntity)
-export class UserRepository extends Repository<UserEntity> {}
+export class UserRepository extends Repository<UserEntity> {
+  async findAccountByEmail(email) {
+    const userData = await this.findOne({
+      where: { email: email },
+    });
+
+    if (!userData) {
+      return false;
+    } else {
+      return userData.registerMethod;
+    }
+  }
+}
