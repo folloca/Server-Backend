0085e4943de175eda307679a672472e54d342622
alsrlqor <skyup9393@gmail.com>
2022-11-25 23:33:51 +0900

    [feat] #12 - Add JWT & Passport module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,6 +1,9 @@
 import { CacheModule, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeormRepositoryModule } from '../../database/typeorm-repository.module';
 import { UserRepository } from '../../database/repositories/user.repository';
 import { AdminRepository } from '../../database/repositories/admin.repository';
@@ -12,6 +15,15 @@ import { SmtpConfig } from '../../config/smtp.config';
       UserRepository,
       AdminRepository,
     ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get(`${process.env.NODE_ENV}.auth.jwt_secret`),
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
+    PassportModule,
     CacheModule.register(),
   ],
   controllers: [AuthController],
