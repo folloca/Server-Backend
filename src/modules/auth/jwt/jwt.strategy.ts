29cdfa7eec158cc7a5b851cb7e1c0e358ed6264b
alsrlqor <skyup9393@gmail.com>
2022-11-27 17:29:02 +0900

    [feat] #12 - Fix validate function

--- src/modules/auth/jwt/jwt.strategy.ts
+++ src/modules/auth/jwt/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
@@ -23,6 +23,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: Payload) {
-    return this.userRepository.getUserData(payload.userId);
+    const userData = this.userRepository.getUserData(payload.userId);
+
+    if (userData) {
+      return userData;
+    } else {
+      throw new UnauthorizedException();
+    }
   }
 }
