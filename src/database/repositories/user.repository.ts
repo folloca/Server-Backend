fdc246825b912669891750ab312bf717fa04a837
alls7554 <alls970459@gmail.com>
2022-12-11 15:49:43 +0900

    [feat] #11 - Add find user by kakao method

--- src/database/repositories/user.repository.ts
+++ src/database/repositories/user.repository.ts
@@ -3,4 +3,15 @@ import { Repository } from 'typeorm';
 import { UserEntity } from '../entities';
 
 @TypeormRepository(UserEntity)
-export class UserRepository extends Repository<UserEntity> {}
+export class UserRepository extends Repository<UserEntity> {
+  async findAccountByKakaoId(kakao_id: string) {
+    const userData = await this.findOne({
+      where: { kakao_id: kakao_id },
+    });
+    if (!userData) {
+      return false;
+    } else {
+      return userData;
+    }
+  }
+}
