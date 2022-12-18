50eb653305bf414093e770e75a101b8dfb0482eb
alsrlqor <skyup9393@gmail.com>
2022-12-18 23:21:32 +0900

    [feat] #17 - Add get trending estate list and create estate data API

--- src/modules/estates/estates.service.ts
+++ src/modules/estates/estates.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { EstateRepository } from '../../database/repositories/estate.repository';
+import { ProposalRepository } from '../../database/repositories/proposal.repository';
+import { CreateEstateDto } from './dto/req/create-estate.dto';
 
 @Injectable()
-export class EstatesService {}
+export class EstatesService {
+  constructor(
+    private estateRepository: EstateRepository,
+    private proposalRepository: ProposalRepository,
+  ) {}
+
+  async getEstateListByPopularity() {
+    const data = await this.estateRepository.getEstatesDataForTrending();
+
+    data.sort((a, b) => {
+      return (
+        b.proposal_count * 2 +
+        b.total_likes -
+        (a.proposal_count * 2 + a.total_likes)
+      );
+    });
+
+    return {
+      data,
+      message: 'Estate list arranged by popularity(참여기획수:좋아요수_2:1)',
+    };
+  }
+
+  async createEstate(createEstateDto: CreateEstateDto) {
+    await this.estateRepository.createEstateData(createEstateDto);
+  }
+}
