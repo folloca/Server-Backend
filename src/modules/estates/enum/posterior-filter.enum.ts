a5e4d7e140c5faf02ee187f799061c3dfe4793dd
alsrlqor <skyup9393@gmail.com>
2022-12-19 23:57:43 +0900

    [feat] #17 - Add filter enums

---
+++ src/modules/estates/enum/posterior-filter.enum.ts
@@ -0,0 +1,5 @@
+export enum PriorFilterEnum {
+  기획참여순 = 'participation',
+  최신순 = 'latest',
+  오래된순 = 'older',
+}
---
+++ src/modules/estates/enum/prior-filter.enum.ts
@@ -0,0 +1,5 @@
+export enum PriorFilterEnum {
+  전체 = 'total',
+  진행중 = 'inProgress',
+  마감 = 'close',
+}
