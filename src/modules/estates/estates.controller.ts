9590d0227f5f9da1ad0f3fd032fd8fa217db0a88
alsrlqor <skyup9393@gmail.com>
2022-12-20 23:22:18 +0900

    [feat] #17 - Add filtered estate list get API

--- src/modules/estates/estates.controller.ts
+++ src/modules/estates/estates.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { EstatesService } from './estates.service';
 import { CreateEstateDto } from './dto/req/create-estate.dto';
+import { PriorFilterEnum } from './enum/prior-filter.enum';
+import { PosteriorFilterEnum } from './enum/posterior-filter.enum';
 
 @ApiTags('estates')
 @Controller('estates')
@@ -10,7 +12,7 @@ export class EstatesController {
 
   @Get('/popularity')
   @ApiOperation({
-    summary: '인기순 조회',
+    summary: '공간 리스트 인기순 조회',
     description: '인기순 정렬 조회(트렌드 페이지)',
   })
   async getEstateListByPopularity() {
@@ -19,11 +21,22 @@ export class EstatesController {
 
   @Get()
   @ApiOperation({
-    summary: '필터 조회',
+    summary: '공간 리스트 필터 조회',
     description: '공간 페이지 필터 조회',
   })
-  async getEstateList() {
-    return this.estatesService.getEstateList();
+  @ApiQuery({
+    name: 'priorFilter',
+    enum: PriorFilterEnum,
+    description: '기획 모집 마감 여부 필터링',
+  })
+  @ApiQuery({
+    name: 'posteriorFilter',
+    enum: PosteriorFilterEnum,
+    description: '시간순 필터링',
+  })
+  async getEstateList(@Query() query) {
+    const { priorFilter, posteriorFilter } = query;
+    return this.estatesService.getEstateList(priorFilter, posteriorFilter);
   }
 
   @Post()
