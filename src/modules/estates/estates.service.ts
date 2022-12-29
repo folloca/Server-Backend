323ccb26a272560c6b6c132cc1a2ec1ef70e897f
alsrlqor <skyup9393@gmail.com>
2022-12-29 23:17:35 +0900

    [refactor] #18 - Get data from query at once

--- src/modules/estates/estates.service.ts
+++ src/modules/estates/estates.service.ts
@@ -15,14 +15,6 @@ export class EstatesService {
   async getEstateListByPopularity() {
     const data = await this.estateRepository.getEstatesDataForTrending();
 
-    data.sort((a, b) => {
-      return (
-        b.proposal_count * 2 +
-        b.total_likes -
-        (a.proposal_count * 2 + a.total_likes)
-      );
-    });
-
     return {
       data,
       message: 'Estate list arranged by popularity(참여기획수:좋아요수_2:1)',
