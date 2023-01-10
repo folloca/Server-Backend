648553baaacad0699c12a239b5db4e163b371a8e
alsrlqor <skyup9393@gmail.com>
2023-01-10 23:43:56 +0900

    [feat] #26 - Add random nickname recommendation API

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { UsersService } from './users.service';
+import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(
+    private readonly configService: ConfigService,
+    private usersService: UsersService,
+  ) {}
+
+  @Get('/nickname/random')
+  @ApiOperation({
+    summary: '닉네임 랜덤 추천',
+    description: '아직 등록되지 않은 닉네임 조합으로 추천',
+  })
+  async getRandomNickname() {
+    return this.usersService.getRandomNickname();
+  }
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { UserRepository } from '../../database/repositories/user.repository';
+import Redis from 'ioredis';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private redis;
+  private readonly adjectives;
+  private readonly nouns;
+
+  constructor(
+    private readonly configService: ConfigService,
+    private userRepository: UserRepository,
+  ) {
+    this.redis = new Redis({
+      host: configService.get(`${process.env.NODE_ENV}.redis.host`),
+      port: configService.get(`${process.env.NODE_ENV}.redis.port`),
+    });
+    this.adjectives = ['형용사1', '형용사2', '형용사3'];
+    this.nouns = ['명사1', '명사2', '명사3'];
+  }
+
+  pickRandomKeyword(array): string {
+    const random = Math.floor(Math.random() * array.length);
+    return array[random];
+  }
+  async getRandomNickname() {
+    const adjective = this.pickRandomKeyword(this.adjectives);
+    const noun = this.pickRandomKeyword(this.nouns);
+    const randomNickname = adjective + noun;
+
+    const nickNameList = await this.redis.smembers('registered_nicknames');
+
+    if (!nickNameList.includes(randomNickname)) {
+      await this.redis.sadd('registered_nicknames', randomNickname);
+      return { randomNickname, message: 'Random unique nickname' };
+    } else {
+      await this.getRandomNickname();
+    }
+  }
+}
