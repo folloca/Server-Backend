1aa4151df34ae67d0d26aced4eec0a28f398bf88
alsrlqor <skyup9393@gmail.com>
2023-01-12 23:54:19 +0900

    [feat] #26 - Create and import keyword data array file

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { UserRepository } from '../../database/repositories/user.repository';
+import { adjectives, nouns } from './nickname-keywords';
 import Redis from 'ioredis';
 
 @Injectable()
 export class UsersService {
   private redis;
-  private readonly adjectives;
-  private readonly nouns;
 
   constructor(
     private readonly configService: ConfigService,
@@ -17,8 +16,6 @@ export class UsersService {
       host: configService.get(`${process.env.NODE_ENV}.redis.host`),
       port: configService.get(`${process.env.NODE_ENV}.redis.port`),
     });
-    this.adjectives = ['형용사1', '형용사2', '형용사3'];
-    this.nouns = ['명사1', '명사2', '명사3'];
   }
 
   pickRandomKeyword(array): string {
@@ -26,14 +23,13 @@ export class UsersService {
     return array[random];
   }
   async getRandomNickname() {
-    const adjective = this.pickRandomKeyword(this.adjectives);
-    const noun = this.pickRandomKeyword(this.nouns);
+    const adjective = this.pickRandomKeyword(adjectives);
+    const noun = this.pickRandomKeyword(nouns);
     const randomNickname = adjective + noun;
 
     const nickNameList = await this.redis.smembers('registered_nicknames');
 
     if (!nickNameList.includes(randomNickname)) {
-      await this.redis.sadd('registered_nicknames', randomNickname);
       return { randomNickname, message: 'Random unique nickname' };
     } else {
       await this.getRandomNickname();
