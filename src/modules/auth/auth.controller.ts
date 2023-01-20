3a4d9622cfe1e0a4ef64a692d6c527c183f9e803
alsrlqor <skyup9393@gmail.com>
2023-01-21 00:21:34 +0900

    [style] Rename OAuth login req DTO file

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -19,7 +19,7 @@ import {
 } from '@nestjs/swagger';
 import { SignupReqDto } from './dto/req/signup-req.dto';
 import { LoginReqDto } from './dto/req/login-req.dto';
-import { OauthSignupReqDto } from './dto/req/oAuthSignup-req.dto';
+import { OauthSignupReqDto } from './dto/req/oauth-signup-req.dto';
 
 @ApiTags('auth')
 @Controller('auth')
