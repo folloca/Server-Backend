c43bd7ba853d69e61275051c8a5976e8cb6f1a60
alsrlqor <skyup9393@gmail.com>
2023-02-12 21:20:59 +0900

    [feat] #40 - Update authorization guard

--- src/modules/auth/jwt/jwt.guard.ts
+++ src/modules/auth/jwt/jwt.guard.ts
@@ -1,5 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from '../auth.service';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private authService: AuthService) {
+    super();
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const { authorization } = request?.headers;
+
+    const tokenValidation = await this.authService.validateAccessToken(
+      authorization,
+    );
+
+    return !!tokenValidation;
+  }
+}
