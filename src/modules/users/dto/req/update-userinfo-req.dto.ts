7bb0abc6a4247af14e62bf0fbcef1339408da990
alsrlqor1007 <skyup9393@gmail.com>
2023-04-05 13:55:10 +0900

    [feat] #49 - Update DTO for optional usage

--- src/modules/users/dto/req/update-userinfo-req.dto.ts
+++ src/modules/users/dto/req/update-userinfo-req.dto.ts
@@ -1,14 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBooleanString, IsNotEmpty } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+function transformToUndefinedIfEmpty() {
+  return Transform(({ value }) => (value === '' ? undefined : value));
+}
 
 export class UpdateUserinfoReqDto {
   @ApiProperty({
-    required: true,
+    required: false,
     type: String,
     format: 'binary',
     description: '프로필 이미지',
   })
-  profileImage: Express.Multer.File;
+  @transformToUndefinedIfEmpty()
+  @IsOptional()
+  profileImage: Express.Multer.File | null;
 
   @ApiProperty({
     required: false,
@@ -16,45 +23,57 @@ export class UpdateUserinfoReqDto {
     maxLength: 50,
     description: '한 줄 소개',
   })
-  baseIntroduction?: string;
+  @transformToUndefinedIfEmpty()
+  @IsString()
+  @IsOptional()
+  baseIntroduction: string | null;
 
   @ApiProperty({
     required: false,
     type: String,
     description: '개인 웹 사이트',
   })
-  websiteUrl?: string;
+  @transformToUndefinedIfEmpty()
+  @IsString()
+  @IsOptional()
+  websiteUrl: string | null;
 
   @ApiProperty({
     required: false,
     type: String,
     description: 'sns url',
   })
-  snsUrl?: string;
+  @transformToUndefinedIfEmpty()
+  @IsString()
+  @IsOptional()
+  snsUrl: string | null;
 
   @ApiProperty({
-    required: true,
+    required: false,
     type: Boolean,
     description: '연락처 공개 여부',
   })
-  @IsBooleanString()
-  contactInfoPublic: boolean;
+  @transformToUndefinedIfEmpty()
+  @IsOptional()
+  contactInfoPublic: boolean | null;
 
   @ApiProperty({
-    required: true,
+    required: false,
     type: String,
     maxLength: 10,
     description: '닉네임',
   })
-  @IsNotEmpty()
-  nickname: string;
+  @transformToUndefinedIfEmpty()
+  @IsString()
+  @IsOptional()
+  nickname: string | null;
 
   @ApiProperty({
-    required: true,
+    required: false,
     type: Boolean,
     description: '마케팅 수신 동의 여부',
   })
-  @IsBooleanString()
-  @IsNotEmpty()
-  marketingReception: boolean;
+  @transformToUndefinedIfEmpty()
+  @IsOptional()
+  marketingReception: boolean | null;
 }
