2266d9f5909614bdfd8fefb62b1be13dae4c1429
alls7554 <alls970459@gmail.com>
2023-04-14 10:56:50 +0900

    [feat] #59 - blacklist check & expired check of token in middleware

--- src/middleware/multer-user-id.middleware.ts
+++ src/middleware/multer-user-id.middleware.ts
@@ -1,15 +1,40 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import Redis from 'ioredis';
 
 @Injectable()
 export class MulterUserIdMiddleware implements NestMiddleware {
-  constructor(private jwtService: JwtService) {}
+  private redis: Redis;
+  constructor(
+    private configService: ConfigService,
+    private jwtService: JwtService,
+  ) {
+    this.redis = new Redis({
+      host: configService.get(`${process.env.NODE_ENV}.redis.host`),
+      port: configService.get(`${process.env.NODE_ENV}.redis.port`),
+      db: configService.get(`${process.env.NODE_ENV}.redis.index.auth`),
+    });
+  }
 
   async use(req: Request, res: Response, next: NextFunction) {
-    const token = req.headers.authorization.replace('Bearer', '').trim();
-    const { userId } = await this.jwtService.verifyAsync(token);
-    req.query.userId = userId;
-    next();
+    const token = req.headers.authorization.replace('Bearer ', '').trim();
+
+    if (await this.redis.get(`black_${token}`)) {
+      res.status(403).send({
+        message: 'Blacklist Token',
+      });
+    }
+
+    try {
+      const { userId } = await this.jwtService.verifyAsync(token);
+      req.query.userId = userId;
+      next();
+    } catch (err) {
+      res.status(401).send({
+        message: 'Expired Token',
+      });
+    }
   }
 }
