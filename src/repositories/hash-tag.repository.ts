70506089d28f78cfe352242b8dc7922a0e16240b
alsrlqor1007 <skyup9393@gmail.com>
2023-04-16 01:56:48 +0900

    [feat] #60 - Add hash-tag repository and methods

--- src/repositories/hash-tag.repository.ts
+++ src/repositories/hash-tag.repository.ts
@@ -1,6 +1,24 @@
 import { TypeormRepository } from '../custom/decorator/typeorm-repository.decorator';
 import { Repository } from 'typeorm';
-import { HashTagEntity } from '../database/entities';
+import { EstateTagEntity, HashTagEntity } from '../database/entities';
 
 @TypeormRepository(HashTagEntity)
-export class HashTagRepository extends Repository<HashTagEntity> {}
+export class HashTagRepository extends Repository<HashTagEntity> {
+  private managerConnection = this.manager.connection;
+
+  async searchHashTag(word: string) {}
+  async createHashTag(word: string[]) {}
+}
+
+@TypeormRepository(EstateTagEntity)
+export class EstateTagRepository extends Repository<EstateTagEntity> {
+  private managerConnection = this.manager.connection;
+
+  async createEstateTag(
+    estateId: number,
+    estateTagId: number,
+    hashTagId: number,
+  ) {
+    // Insert
+  }
+}
