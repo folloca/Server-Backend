82ce3d4c1be2675b890146eea932cb860f10f652
alsrlqor1007 <skyup9393@gmail.com>
2023-04-21 23:32:37 +0900

    [feat] #69 - Import modules and add middleware

--- src/modules/proposals.module.ts
+++ src/modules/proposals.module.ts
@@ -1,4 +1,9 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { ProposalsController } from '../controllers/proposals.controller';
 import { ProposalsService } from '../services/proposals.service';
 import { TypeormRepositoryModule } from './typeorm-repository.module';
@@ -10,6 +15,10 @@ import {
   ProposalRepository,
 } from '../repositories/proposal.repository';
 import { HashTagRepository } from '../repositories/hash-tag.repository';
+import { MulterModule } from '@nestjs/platform-express';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RedisModule } from '@nestjs-modules/ioredis';
+import { MulterUserIdMiddleware } from '../middleware/multer-user-id.middleware';
 
 @Module({
   imports: [
@@ -21,9 +30,46 @@ import { HashTagRepository } from '../repositories/hash-tag.repository';
       OpinionRepository,
       HashTagRepository,
     ]),
+    MulterModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        dest: configService.get(`${process.env.NODE_ENV}.storage.proposal`),
+      }),
+      inject: [ConfigService],
+    }),
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const host = await configService.get(
+          `${process.env.NODE_ENV}.redis.host`,
+        );
+        const port = await configService.get(
+          `${process.env.NODE_ENV}.redis.port`,
+        );
+
+        const index = await configService.get(
+          `${process.env.NODE_ENV}.redis.index.proposal`,
+        );
+
+        return {
+          config: {
+            url: `redis://${host}:${port}`,
+            db: index,
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
   ],
   controllers: [ProposalsController],
   providers: [ProposalsService],
   exports: [ProposalsService],
 })
-export class ProposalsModule {}
+export class ProposalsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(MulterUserIdMiddleware).forRoutes({
+      path: 'proposals',
+      method: RequestMethod.POST,
+    });
+  }
+}
