c9ac7454a7c5aff82903c74f7c8b1e5ca074d668
alsrlqor1007 <skyup9393@gmail.com>
2023-04-22 19:53:58 +0900

    [feat] #69 - Add proposal data insert query method

--- src/repositories/proposal.repository.ts
+++ src/repositories/proposal.repository.ts
@@ -9,7 +9,26 @@ import {
 } from '../database/entities';
 
 @TypeormRepository(ProposalEntity)
-export class ProposalRepository extends Repository<ProposalEntity> {}
+export class ProposalRepository extends Repository<ProposalEntity> {
+  async createProposalData(
+    userId: number,
+    thumbnail: string,
+    estateId: number,
+    proposalIntroduction: string,
+    proposalDescription: string,
+    opinionOpen: boolean,
+  ) {
+    const data = {
+      proposalIntroduction,
+      proposalDescription,
+      opinionOpen,
+      thumbnail,
+      estateId,
+      plannerId: userId,
+    };
+    return await this.save(data);
+  }
+}
 
 @TypeormRepository(ProposalDetailEntity)
 export class ProposalDetailRepository extends Repository<ProposalDetailEntity> {}
