2106518209e006403b4564acda653dd1df38e8ea
alsrlqor1007 <skyup9393@gmail.com>
2023-04-24 10:36:47 +0900

    [feat] #69 - Add proposal creation service method

--- src/services/proposals.service.ts
+++ src/services/proposals.service.ts
@@ -1,4 +1,84 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRedis } from '@nestjs-modules/ioredis';
+import { ConfigService } from '@nestjs/config';
+import {
+  ProposalDetailRepository,
+  ProposalLikeRepository,
+  ProposalRepository,
+} from '../repositories/proposal.repository';
+import { ProposalImageRepository } from '../repositories/image.repository';
+import {
+  HashTagRepository,
+  ProposalTagRepository,
+} from '../repositories/hash-tag.repository';
+import { EstateRepository } from '../repositories/estate.repository';
+import { CreateProposalDto } from '../dto/req/create-proposal.dto';
+import Redis from 'ioredis';
 
 @Injectable()
-export class ProposalsService {}
+export class ProposalsService {
+  constructor(
+    @InjectRedis()
+    private readonly redis: Redis,
+    private readonly configService: ConfigService,
+    private proposalRepository: ProposalRepository,
+    private proposalLikeRepository: ProposalLikeRepository,
+    private proposalImageRepository: ProposalImageRepository,
+    private proposalDetailRepository: ProposalDetailRepository,
+    private proposalTagRepository: ProposalTagRepository,
+    private hashTagRepository: HashTagRepository,
+    private estateRepository: EstateRepository,
+  ) {}
+
+  async createProposal(
+    userId: number,
+    filenames: { thumbnail: string; images: string[] },
+    createProposalDto: CreateProposalDto,
+  ) {
+    const { thumbnail, images = [] } = filenames;
+    const {
+      estateId,
+      proposalIntroduction,
+      proposalDescription,
+      proposalDetails,
+      opinionOpen,
+      hashTag1,
+      hashTag2,
+    } = createProposalDto;
+
+    try {
+      const { proposalId } = await this.proposalRepository.createProposalData(
+        userId,
+        thumbnail,
+        +estateId,
+        proposalIntroduction,
+        proposalDescription,
+        opinionOpen,
+      );
+
+      await this.estateRepository.updateProposalCount(+estateId, 1);
+
+      const validFilenames = [thumbnail, ...images].filter(Boolean);
+      await this.proposalImageRepository.createImageData(
+        proposalId,
+        validFilenames,
+      );
+
+      await this.proposalDetailRepository.createDetailData(
+        proposalId,
+        proposalDetails,
+      );
+
+      if (hashTag1 || hashTag2) {
+        const tagResult = await this.hashTagRepository.createHashTag(
+          [hashTag1, hashTag2].filter(Boolean),
+        );
+        await this.proposalTagRepository.createEstateTag(proposalId, tagResult);
+      }
+
+      return { message: `Registered new proposal of estate ${estateId}` };
+    } catch (e) {
+      Logger.error(e);
+    }
+  }
+}
