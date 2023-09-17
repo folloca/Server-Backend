9de050c48fea83e6c6a21fb4eea22eda68a6128e
alsrlqor <skyup9393@gmail.com>
2023-09-18 08:59:54 +0900

    Set PM2 cluster mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,12 @@ module.exports = {
     {
       name: 'folloca-server',
       script: './dist/main.js',
-      watch: false,
+      instances: 4,
+      exec_mode: 'cluster',
+      merge_logs: true,
+      autorestart: true,
+      watch: true,
+      instance_var: 'INSTANCE_ID',
       env_development: {
         NODE_ENV: 'development',
       },
